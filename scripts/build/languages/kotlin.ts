3cf865705a36293560df69fd0e62b22f76b270be
Joe Polny <joepolny@gmail.com>
2025-06-04 15:01:09 -0400

    use cargo ndk

--- scripts/build/languages/kotlin.ts
+++ scripts/build/languages/kotlin.ts
@@ -1,19 +1,11 @@
 import { toPascalCase, run } from "..";
 
 export async function buildKotlin(crate: string) {
-  const targets = [
-    // "aarch64-linux-android"
-  ];
-
-  let cargoBuildCmd = `cargo --color always build --manifest-path crates/${crate}_ffi/Cargo.toml`;
-
-  targets.map((target) => {
-    cargoBuildCmd += ` --target ${target}`;
-  });
+  let cargoBuildCmd = `cargo ndk --manifest-path crates/${crate}_ffi/Cargo.toml build`;
 
   await run(cargoBuildCmd);
 
   await run(
-    `cargo --color always run -p uniffi-bindgen generate --no-format --library target/debug/lib${crate}_ffi.dylib --language kotlin --out-dir target/debug/kotlin/${crate}`,
+    `cargo --color always run -p uniffi-bindgen generate --no-format --library target/aarch64-linux-android/debug/lib${crate}_ffi.so --language kotlin --out-dir target/debug/kotlin/${crate}`,
   );
 }
